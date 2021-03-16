15500eaae817e84f9caf6b5c505cbdfd29cd8c5b
Ed Summers <ehs@pobox.com>
2021-03-16 17:49:38 -0400

    shorter

--- diff.py
+++ diff.py
@@ -7,9 +7,10 @@ import datetime
 df1 = pandas.read_csv('data-2021-02-28.csv', parse_dates=['VACCINATION_DATE'])
 df2 = pandas.read_csv('data-2021-03-04.csv', parse_dates=['VACCINATION_DATE'])
 
-# limit to dates prior to December 11, 2020
-df1 = df1[df1['VACCINATION_DATE'] < datetime.datetime(2020, 12, 11, tzinfo=pytz.UTC)]
-df2 = df2[df2['VACCINATION_DATE'] < datetime.datetime(2020, 12, 11, tzinfo=pytz.UTC)]
+# limit analysis to dates prior to December 11, 2020
+cutoff = datetime.datetime(2020, 12, 11, tzinfo=pytz.UTC)
+df1 = df1[df1['VACCINATION_DATE'] < cutoff]
+df2 = df2[df2['VACCINATION_DATE'] < cutoff]
 
 # lets just compare dates to see what rows were added or deleted
 df1 = df1[['VACCINATION_DATE']]
