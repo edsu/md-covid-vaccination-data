86d575075b96f37a6ca95f54cd2034e6d740cc4e
Ed Summers <ehs@pobox.com>
2021-03-16 17:47:06 -0400

    removed dead code

--- diff.py
+++ diff.py
@@ -3,18 +3,17 @@
 import pytz
 import pandas
 import datetime
+
 df1 = pandas.read_csv('data-2021-02-28.csv', parse_dates=['VACCINATION_DATE'])
 df2 = pandas.read_csv('data-2021-03-04.csv', parse_dates=['VACCINATION_DATE'])
 
 # limit to dates prior to December 11, 2020
-
 df1 = df1[df1['VACCINATION_DATE'] < datetime.datetime(2020, 12, 11, tzinfo=pytz.UTC)]
 df2 = df2[df2['VACCINATION_DATE'] < datetime.datetime(2020, 12, 11, tzinfo=pytz.UTC)]
 
-# see what rows were addeded or deleted
-df1 = df1[['VACCINATION_DATE']] #, 'DailyFirstDose', 'DailySecondDose']]
-df2 = df2[['VACCINATION_DATE']] #, 'DailyFirstDose', 'DailySecondDose']]
-
+# lets just compare dates to see what rows were added or deleted
+df1 = df1[['VACCINATION_DATE']]
+df2 = df2[['VACCINATION_DATE']]
 merged = df1.merge(df2, indicator=True, how='outer')
 merged = merged.sort_values('VACCINATION_DATE')
 
@@ -23,6 +22,4 @@ for i, row in merged.iterrows():
         print('new row', row['VACCINATION_DATE'])
     elif row['_merge'] == 'left_only':
         print('deleted row', row['VACCINATION_DATE'])
-
-
 
