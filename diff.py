8471f3cdfff9f3cbb6b1ab0850905365cba8f87e
Ed Summers <ehs@pobox.com>
2021-03-16 13:30:00 -0400

    simplified

--- diff.py
+++ diff.py
@@ -11,7 +11,7 @@ df2 = pandas.read_csv('data-2021-03-04.csv', parse_dates=['VACCINATION_DATE'])
 df1 = df1[df1['VACCINATION_DATE'] < datetime.datetime(2020, 12, 11, tzinfo=pytz.UTC)]
 df2 = df2[df2['VACCINATION_DATE'] < datetime.datetime(2020, 12, 11, tzinfo=pytz.UTC)]
 
-# just include the daily counts
+# see what rows were addeded or deleted
 df1 = df1[['VACCINATION_DATE']] #, 'DailyFirstDose', 'DailySecondDose']]
 df2 = df2[['VACCINATION_DATE']] #, 'DailyFirstDose', 'DailySecondDose']]
 
